d00b98bb1ff91b56c1f91d01eeee9f01de9f0526
Seonghwan Lee <uplsh580@gmail.com>
2021-02-16 00:07:10 +0000

    Gen._Random_Data : add csv_output func.

--- GRD/gen_data.py
+++ GRD/gen_data.py
@@ -1,5 +1,6 @@
 import os 
 import json
+import csv
 
 from utils.col import *
 
@@ -31,13 +32,36 @@ def parser(path):
 
         cols.append(col)
         col_names.append(c_name)
-    
-    for i, col in enumerate(cols):
-        print(col_names[i], ":", col.gen_data())
+  
+    # # TEST
+    # for i, col in enumerate(cols):
+    #     print(col_names[i], ":", col.gen_data())
+  
+    return col_names, cols
+
+
+def csv_output(rows:int, col_names:list, cols:list, output_path:str, file_name, with_header:bool=True) -> None:
+    if output_path != '' and output_path[-1] != '/':
+        output_path += '/'
+
+    if len(col_names) != len(cols):
+        raise Exception('Schema Error: "col_names" len different from "cols" len')
+    with open(output_path+file_name,'w', newline='') as outcsv:
+        wr = csv.writer(outcsv)
+
+        if with_header:
+            wr.writerow(col_names)
+        
+        for i in range(rows):
+            row = map(lambda col : col.gen_data(), cols)
+            wr.writerow(row)
+    outcsv.close()
+
 
 if __name__ == '__main__':
     path = "./configs/example.json"
     abs_path = os.path.abspath(path)
     
-    parser(abs_path)
+    col_names, cols = parser(abs_path)
+    csv_output(10, col_names, cols, './output/', 'example.csv')
     pass
