b8fcc415a5a7ff32623282c8e64130430110646e
Seonghwan Lee <uplsh580@gmail.com>
2021-02-21 00:08:54 +0000

    fix bug

--- gen_data.py
+++ gen_data.py
@@ -3,14 +3,14 @@ import yaml
 import csv
 import argparse
 from operator import itemgetter
-from utils.col import *
+from utils.col import COL_INT, COL_REGEX, COL_LIST, COL_DATETIME
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-n', required=True, dest='row_number', type=int,
                     help='Number of rows')
 parser.add_argument('-f', required=True, dest='file_path', type=str,
                     help='config file(YAML) path')
-parser.add_argument('-o', dest='outfile_dir', type=str, default="output/"
+parser.add_argument('-o', dest='outfile_dir', type=str, default="output/",
                     help='output file directory')
 parser.add_argument('--noheader', action='store_true',
                     help='remove header line in output csv file')
@@ -53,7 +53,7 @@ def parser(path):
 def csv_output(n: int, col_names: list, col_instances: list, outfile_path: str,
                with_header: bool = True,  order_key: str = None) -> None:
     if len(col_names) != len(col_instances):
-        raise Exception(f'[Internal Error] "col_names({len(col_names)})" len different from "cols" len({len(cols)})')
+        raise Exception(f'[Internal Error] "col_names({len(col_names)})" len different from "cols" len({len(col_instances)})')
     if order_key is not None and order_key not in col_names:
         raise Exception(f'[Config Error] "order_key({order_key})" is not in "col_names({col_names})"')
 
@@ -74,7 +74,7 @@ def csv_output(n: int, col_names: list, col_instances: list, outfile_path: str,
 
 
 if __name__ == '__main__':
-    outfile_dir = os.path.abspath((args.outfile_dir)
+    outfile_dir = os.path.abspath(args.outfile_dir)
     outfile_name = os.path.basename(args.file_path)
     outfile_name = outfile_name.replace(".yml", ".csv").replace(".yaml", ".csv")
 
